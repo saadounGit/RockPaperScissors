8df9b6f9832b755630e203a42bffe9a145f34eda
Saadoun Alerris <saadoun@protonmail.com>
2022-07-20 15:54:19 +0300

    Initial Commit

---
+++ RockPaperScissors/RockPaperScissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperScissorsApp.swift
+//  RockPaperScissors
+//
+//  Created by Saadoun Alerris on 20/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct RockPaperScissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
